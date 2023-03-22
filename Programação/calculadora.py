66be3b5eb7e8c405f6fe6137cd8dc55134c14828
BinottoA <127997930+BinottoA@users.noreply.github.com>
2023-03-22 18:53:37 -0300

    Add files via upload

---
+++ Programação/calculadora.py
@@ -0,0 +1,12 @@
+a=float(input())         #float = números decimais 
+c=input()
+b=float(input())           #int = números inteiros
+
+if c=="+": 
+    print(a+b)
+if c=="-": 
+    print(a-b)
+if c=="*": 
+    print(a*b)
+if c=="/": 
+    print(a/b)
