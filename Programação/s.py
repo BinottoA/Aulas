ce28caf029e85e1b1673901dc2b7efa52921daec
BinottoA <127997930+BinottoA@users.noreply.github.com>
2023-05-31 18:52:21 -0300

    Add files via upload

---
+++ Programação/s.py
@@ -0,0 +1,10 @@
+lista=[ "Alexandre","Alice","André","Arthur","Arthur","Artur","Augusto","Bernardo","Bernardo","Bruno","Davi","Diego","Eduardo","Fabrício","Felipe","Fernando","Francisco","Francisco","Gabriel","Gabriel","Giovanna","Giovanni","Guilherme","Guilherme","Hector","Henrique","Inácio","João","João","Joaquim","Júlia","Lauren","Leonardo","Leonardo","Lucas","Marina","Matheus","Matheus","Paula","Pedro","Pedro","Pedro","Pedro","Rafael","Regis","Sofia","Stella","Thiago","Valentina","Vicente","Lucas"]
+
+
+nova_lista = []
+for item in lista:
+    nova_lista.append("Vitor " + item)
+
+#Mostra o resultado da nova lista
+for item in nova_lista:
+    print(item + "\n")
